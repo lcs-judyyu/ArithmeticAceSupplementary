100e28d274a05471c3f4cbed63358dfcb92afe39
Judy <jyu@lcs.on.ca>
2022-02-28 08:58:05 -0500

    Change the app to show the tab view

--- ArithmeticAce/ArithmeticAceApp.swift
+++ ArithmeticAce/ArithmeticAceApp.swift
@@ -10,36 +10,33 @@ import SwiftUI
 @main
 struct ArithmeticAceApp: App {
     var body: some Scene {
-//        WindowGroup {
-//
-//            TabView {
-//                AdditionView()
-//                    .tabItem {
-//                        Image(systemName: "plus.circle")
-//                        Text("Add")
-//                    }
-//
-//                SubtractionView()
-//                    .tabItem {
-//                        Image(systemName: "minus.circle")
-//                        Text("Subtract")
-//                    }
-//
-//                MultiplicationView()
-//                    .tabItem {
-//                        Image(systemName: "multiply.circle")
-//                        Text("Multiply")
-//                    }
-//
-//                DivisionView()
-//                    .tabItem {
-//                        Image(systemName: "divide.circle")
-//                        Text("Divide")
-//                    }
-//            }
-//        }
         WindowGroup {
-            AdditionView()
+
+            TabView {
+                AdditionView()
+                    .tabItem {
+                        Image(systemName: "plus.circle")
+                        Text("Add")
+                    }
+
+                SubtractionView()
+                    .tabItem {
+                        Image(systemName: "minus.circle")
+                        Text("Subtract")
+                    }
+
+                MultiplicationView()
+                    .tabItem {
+                        Image(systemName: "multiply.circle")
+                        Text("Multiply")
+                    }
+
+                DivisionView()
+                    .tabItem {
+                        Image(systemName: "divide.circle")
+                        Text("Divide")
+                    }
+            }
         }
     }
 }
